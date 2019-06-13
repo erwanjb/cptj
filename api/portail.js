445fb33e17f3accc267f78eaa4836e751562c43d
erwanjb <wawan.jb@gmail.com>
2019-06-13 09:07:56 +0200

    sha256

--- api/portail.js
+++ api/portail.js
@@ -6,14 +6,13 @@ module.exports = (app) => {
 
 	const portailAPI = (ap) =>{
 		ap.post("/portail", (req,res)=>{
-            var password = 'teyfdsiu';
-            var encrypted = CryptoJS.AES.encrypt(req.body.u.password, password).toString();
-            var decrypted  = CryptoJS.AES.decrypt(encrypted, password).toString(CryptoJS.enc.Utf8);
+            var encrypted = CryptoJS.SHA256(req.body.u.password).toString();
+            console.log(encrypted)
             const r = "SELECT * FROM connexion WHERE identifiant="+mysql.escape(req.body.u.identifiant);
 			connection.query(r, (error, results, fields) => {
         		if (error) throw error;
         		else{
-        			if(CryptoJS.AES.decrypt(results[0].motdepasse, password).toString(CryptoJS.enc.Utf8) === decrypted){
+        			if(results[0].motdepasse === encrypted){
         				req.session.adminConnected=true;
                         const loged = {
                             token:true
