04cb25afd6fc483340727aa8d31ce647489124c4
erwanjb <wawan.jb@gmail.com>
2019-08-11 01:48:12 +0200

    change again

--- api/api.js
+++ api/api.js
@@ -75,18 +75,34 @@ const projet = require('./projet')(app);
 
 https.createServer(options, app).listen(port) */
 
-require("greenlock-express")
-    .create({
-        email: "wawan.jb@gmail.com", // The email address of the ACME user / hosting provider
-        agreeTos: true, // You must accept the ToS as the host which handles the certs
-        configDir: "~/acme/", // Writable directory where certs will be saved
-        communityMember: true, // Join the community to get notified of important updates
-        telemetry: true, // Contribute telemetry data to the project
- 
-        // Using your express app:
-        // simply export it as-is, then include it here
-        app: app
- 
-        //, debug: true
-    })
-    .listen(8080, port);
+var Greenlock = require('greenlock-express')
+
+var greenlock = Greenlock.create({
+	// Let's Encrypt v2 is ACME draft 11
+	version: "draft-11",
+
+	server: "https://acme-v02.api.letsencrypt.org/directory",
+	// Note: If at first you don't succeed, stop and switch to staging
+	// https://acme-staging-v02.api.letsencrypt.org/directory
+
+	// You MUST change this to a valid email address
+	email: "wawan.jb@gmail.com",
+
+	// You MUST NOT build clients that accept the ToS without asking the user
+	agreeTos: true,
+
+	// You MUST change these to valid domains
+	// NOTE: all domains will validated and listed on the certificate
+	approvedDomains: ["cachepastajoie.fr", "www.cachepastajoie.fr"],
+
+	// You MUST have access to write to directory where certs are saved
+	// ex: /home/foouser/acme/etc
+	configDir: "~/acme/",
+
+	// Get notified of important updates and help me make greenlock better
+	communityMember: true
+
+	//, debug: true
+});
+
+https.createServer(greenlock.tlsOptions, app).listen(port)
